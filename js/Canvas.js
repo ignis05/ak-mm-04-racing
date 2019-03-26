a0dd5e0ca7ecffeaac8a617af82bfd9b5ddc4b14
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-03-26 14:15:08 +0100

    Add track rendering

--- js/Canvas.js
+++ js/Canvas.js
@@ -3,9 +3,20 @@ class Canvas {
         this.settings = settings
         this.canvas = canvas
         this.ctx = canvas.getContext('2d');
+        this.radius = (this.canvas.height / 2) - (this.ctx.lineWidth / 2)
         this.addListeners()
+        this.render()
     }
-    addListeners(){
+
+    render() {
+        var rendering = () => {
+            this.renderTrack()
+            requestAnimationFrame(rendering)
+        }
+        rendering()
+    }
+
+    addListeners() {
         // handles multiple key controling by passing their status to settings.keyspressed[player] (keyspressed array starts with 1)
 
         document.addEventListener("keydown", e => {
@@ -14,12 +25,64 @@ class Canvas {
                 this.settings.keyspressed[player] = true;
             }
         })
-    
-    document.addEventListener("keyup", e => {
-        if (this.settings.keybinds.includes(e.code)) {
-            let player = this.settings.keybinds.indexOf(e.code)
-            this.settings.keyspressed[player] = false;
-        }
-    })
+
+        document.addEventListener("keyup", e => {
+            if (this.settings.keybinds.includes(e.code)) {
+                let player = this.settings.keybinds.indexOf(e.code)
+                this.settings.keyspressed[player] = false;
+            }
+        })
+    }
+
+    renderTrack() {
+        this.renderBg()
+        this.renderOuterTrack()
+        this.renderInnerTrack()
+    }
+    renderBg() {
+        let ctx = this.ctx
+        let canvas = this.canvas
+
+        ctx.beginPath();
+        ctx.moveTo(0, 0)
+        ctx.lineTo(canvas.width, 0)
+        ctx.lineTo(canvas.width, canvas.height)
+        ctx.lineTo(0, canvas.height)
+        ctx.closePath()
+
+        ctx.fillStyle = "green";
+        ctx.fill()
+    }
+    renderOuterTrack() {
+        let ctx = this.ctx
+        let canvas = this.canvas
+
+        ctx.beginPath();
+        ctx.lineWidth = 6;
+        let rightDist = canvas.width - (canvas.height / 2)
+        ctx.arc((canvas.height / 2), (canvas.height / 2), (this.radius), (Math.PI / 2), (-Math.PI / 2));
+        ctx.arc(rightDist, (canvas.height / 2), (this.radius), (-Math.PI / 2), (Math.PI / 2));
+        ctx.closePath();
+
+        ctx.strokeStyle = "rgba(0,0,0,1)";
+        ctx.stroke();
+        ctx.fillStyle = "white";
+        ctx.fill()
+    }
+    renderInnerTrack() {
+        let ctx = this.ctx
+        let canvas = this.canvas
+
+        ctx.beginPath();
+        ctx.lineWidth = 6;
+        let rightDist = canvas.width - (canvas.height / 2)
+        ctx.arc((canvas.height / 2), (canvas.height / 2), (this.radius / 3), (Math.PI / 2), (-Math.PI / 2));
+        ctx.arc(rightDist, (canvas.height / 2), (this.radius / 3), (-Math.PI / 2), (Math.PI / 2));
+        ctx.closePath();
+
+        ctx.strokeStyle = "rgba(0,0,0,1)";
+        ctx.stroke();
+        ctx.fillStyle = "green";
+        ctx.fill()
     }
 }
