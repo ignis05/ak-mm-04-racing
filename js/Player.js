fd0656976255ae57a1066dc4b755d26f5969b90d
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-03-28 09:25:36 +0100

    Add trails

--- js/Player.js
+++ js/Player.js
@@ -5,16 +5,30 @@ class Player {
         this.velocity = 2
         this.direction = Math.PI / 2
         this.ctx = ctx
+        this.trail = new Array()
     }
     updatePos() {
         this.posX += this.velocity * Math.sin(this.direction)
         this.posY += this.velocity * Math.cos(this.direction)
+        this.trail.push({ x: this.posX, y: this.posY })
     }
     turn() {
         this.direction += 0.05
     }
     draw() {
         this.ctx.fillStyle = "black"
-        this.ctx.fillRect(this.posX, this.posY, 10, 10);
+        this.ctx.fillRect(this.posX - 5, this.posY - 5, 10, 10);
+        this.drawTrail()
+    }
+    drawTrail() {
+        let ctx = this.ctx
+        ctx.beginPath();
+        ctx.lineWidth = 10;
+        this.trail.forEach(cords => {
+            ctx.lineTo(cords.x, cords.y)
+        })
+        // ctx.closePath()
+        ctx.strokeStyle = "#000000"
+        ctx.stroke()
     }
 }
